6cec257bb917c75aa5002d4c1078d71acc13073d
Anthony Depace <adepace@umass.edu>
2016-11-14 17:09:25 -0500

    made the homepage graph a react component

---
+++ app/components/text_graph.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class TextGraph extends React.Component {
+
+  render() {
+    return (
+      <div>
+          <img src="../img/main_mock_1.png" className="img-responsive" />
+      </div>
+    );
+  }
+}
