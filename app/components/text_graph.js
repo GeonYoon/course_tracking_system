550d3f7b101189dd20c3a18ecb18c7a10852d5ed
Anthony Depace <adepace@umass.edu>
2016-11-14 17:48:29 -0500

    Text Graph for all classes and prereqs

--- app/components/text_graph.js
+++ app/components/text_graph.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import {getCollectionData} from '../server.js';
 
 export default class TextGraph extends React.Component {
 
   render() {
+    var courses = getCollectionData('courses');
+
+
+    var nodes = [];
+    for (var k1 in courses){
+      var node = "\tCourse "+ k1 + " - " + courses[k1]['department'] + courses[k1]['number']+" "+courses[k1]['name'];
+          nodes.push(<p>{node}</p>);
+    }
+    var edges = [];
+    for (var k2 in courses){
+        for (var p in courses[k2]['prereqs']){
+          var edge = "\tCourse "+ courses[k2]['prereqs'][p] + " --> "+k2;
+          edges.push(<p>{edge}</p>);
+        }
+    }
+    
     return (
       <div>
-          <img src="../img/main_mock_1.png" className="img-responsive" />
+        <p>NODES:</p>
+        {nodes}
+        <p>EDGES:</p>
+        {edges}
       </div>
     );
   }
