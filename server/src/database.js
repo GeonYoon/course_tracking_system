08b156de2b2651c6c04f44ea3e40ca9ee61e5125
Michael Keegan <mekeegan@umass.edu>
2016-11-27 17:38:59 -0500

    started setting up server/database

--- server/src/database.js
+++ server/src/database.js
@@ -1,5 +1,200 @@
 // Your startup's initial mock objects go here
 var initialData = {
+  "users":{
+    "1":{
+      "_id":1,
+      "fullName": "Student One",
+      "classesTaken":[1,2,4,5,6],
+      "sId":12345678,
+      "savedGraphs":1,
+      "majors":[1,3],
+      "minors":[2],
+      "gradDate":"May 2018",
+      "email":"sone@umass.edu",
+      "nextSemester":[]
+    }
+  },
+  "accountData":{
+    "username": "sone@umass.edu",
+    "password": "Pasword1"
+  },
+  "feedback":{
+    "1":{
+      "user": 1,
+      "content": "first feedback"
+    }
+  },
+  "majors":{
+    "1":{
+      "title": "Computer Science",
+      "courses":[1,2,3,4,5,6,7,8,9,10,11,13]
+    },
+    "2":{
+      "title": "Math",
+      "courses":[3,4]
+    },
+    "3":{
+      "title": "Philosophy",
+      "courses":[12]
+    }
+  },
+  "courses":{
+    "1":{
+      "id": 1,
+      "name": "Web Programming",
+      "description": "THIS IS A DESCRIPTION",
+      "department": "CS",
+      "number": 326,
+      "prereqs": [2],
+      "textbooks": ["Web Programming for Nerds"]
+    },
+    "2":{
+      "id": 2,
+      "name": "Programming Methodology",
+      "description": "THIS IS A DESCRIPTION",
+      "department": "CS",
+      "number": 220,
+      "prereqs": [5],
+      "textbooks": ["Scala 4 Kidz"]
+    },
+    "3":{
+      "id": 3,
+      "name": "Calculus II",
+      "description": "THIS IS A DESCRIPTION",
+      "department": "MATH",
+      "number": 132,
+      "prereqs": [4],
+      "textbooks": ["INTEGRATE THIS"]
+    },
+    "4":{
+      "id": 4,
+      "name": "Calculus I",
+      "description": "THIS IS A DESCRIPTION",
+      "department": "MATH",
+      "number": 131,
+      "prereqs": [],
+      "textbooks": ["DERIVE THIS"]
+    },
+    "5":{
+      "id": 5,
+      "name": "Programming with Data Structures",
+      "description": "THIS IS A DESCRIPTION",
+      "department": "CS",
+      "number": 187,
+      "prereqs": [6],
+      "textbooks": ["How do i linked list?"]
+    },
+    "6":{
+      "id": 6,
+      "name": "Introduction to Problem Solving",
+      "description": "THIS IS A DESCRIPTION",
+      "department": "CS",
+      "number": 121,
+      "prereqs": [],
+      "textbooks": ["Gordon 'G Money' Anderson: A tour of Umass CS"]
+    },
+    "7":{
+      "id": 7,
+      "name": "Reasoning Under Uncertainty",
+      "description": "THIS IS A DESCRIPTION",
+      "department": "CS",
+      "number": 240,
+      "prereqs": [5],
+      "textbooks": ["Stats with Computers"]
+    },
+    "8":{
+      "id": 8,
+      "name": "Computer Systems Principles",
+      "description": "THIS IS A DESCRIPTION",
+      "department": "CS",
+      "number": 230,
+      "prereqs": [5],
+      "textbooks": ["C Compilers"]
+    },
+    "9":{
+      "id": 9,
+      "name": "Introduction to Computation",
+      "description": "THIS IS A DESCRIPTION",
+      "department": "CS",
+      "number": 250,
+      "prereqs": [5],
+      "textbooks": ["Computers are whack, yo"]
+    },
+    "10":{
+      "id": 10,
+      "name": "Algorithms",
+      "description": "THIS IS A DESCRIPTION",
+      "department": "CS",
+      "number": 311,
+      "prereqs": [9],
+      "textbooks": ["Algorithm Design"]
+    },
+    "11":{
+      "id": 11,
+      "name": "CS Junior Year Writing",
+      "description": "THIS IS A DESCRIPTION",
+      "department": "CS",
+      "number": 305,
+      "prereqs": [],
+      "textbooks": ["none"]
+    },
+    "12":{
+      "id": 12,
+      "name": "Medical Ethics",
+      "description": "THIS IS A DESCRIPTION",
+      "department": "PHIL",
+      "number": 164,
+      "prereqs": [],
+      "textbooks": ["none"]
+    },
+    "13":{
+      "id": 13,
+      "name": "Artificial Intelligence",
+      "description": "THIS IS A DESCRIPTION",
+      "department": "CS",
+      "number": 383,
+      "prereqs": [2,7],
+      "textbooks": ["none"]
+    }
+},
+
+"savePage": {
+  "1": {
+    "_id": 1,
+    "pages": [
+        {
+          "name": "first draft",
+          "time": 1479258637215,
+          "image": "main_mock_1.png"
+        },
+        {
+          "name": "second draft",
+          "time": 1479258638865,
+          "image": "main_mock_1.png"
+        },
+        {
+          "name": "third draft",
+          "time": 1479258639666,
+          "image": "main_mock_1.png"
+        },
+        {
+          "name": "fourth draft",
+          "time": 1479258640143,
+          "image": "main_mock_1.png"
+        },
+        {
+          "name": "fifth draft",
+          "time": 1479258640645,
+          "image": "main_mock_1.png"
+        },
+        {
+          "name": "sixth draft",
+          "time": 1479258641155,
+          "image": "main_mock_1.png"
+        }
+      ]
+  }
+}
 
 };
 
--- server/src/server.js
+++ server/src/server.js
@@ -1,2 +1,28 @@
 // Implement your server in this file.
 // We should be able to run your server with node src/server.js
+var database = require('./database');
+var readDocument = database.readDocument;
+var writeDocument = database.writeDocument;
+var addDocument = database.addDocument;
+
+var bodyParser = require('body-parser');
+
+// Imports the express Node module.
+var express = require('express');
+// Creates an Express server.
+var app = express();
+// Support receiving text in HTTP request bodies
+app.use(bodyParser.text());
+// Support receiving JSON in HTTP request bodies
+app.use(bodyParser.json());
+
+app.use(express.static('../client/build'));
+
+// Defines what happens when it receives the `GET /` request
+app.get('/', function (req, res) {
+res.send('Hello World!');
+});
+// Starts the server on port 3000!
+app.listen(3000, function () {
+console.log('Example app listening on port 3000!');
+});
